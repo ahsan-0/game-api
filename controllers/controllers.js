4942476e3bdf44392e4e7f242cba31ce2e94962d
ahsan-0 <ashanb995@gmail>
2022-12-12 12:14:51 +0000

    send response with categories

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,11 @@
-const { selectCategories } = require("../models/models")
+const { selectCategories } = require("../models/models");
 
 exports.getCategories = (req, res, next) => {
   selectCategories()
-}
+    .then((categories) => {
+      res.status(200).send({ categories });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
