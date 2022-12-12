7a2e14b12d7939f8af7b13cb5a5a72654db6b3da
ahsan-0 <ashanb995@gmail>
2022-12-12 11:59:01 +0000

    create test block for first GET endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,14 @@
 const request = require("supertest");
-const app = require("../app")
-const testData = require("../db/data/test-data")
+const app = require("../app");
+const testData = require("../db/data/test-data/index");
 
-describe("")
+describe("GET /api/categories", () => {
+  test("should respond with 200 and get all categories from test data", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toEqual(testData.categoryData);
+      });
+  });
+});
