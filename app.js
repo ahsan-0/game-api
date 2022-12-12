e0571bff8c6158eb72ace16a74f29c550202b745
ahsan-0 <ashanb995@gmail>
2022-12-12 15:21:43 +0000

    remove app.use(express.json) until i actually need it

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
-const { getCategories } = require("./controllers/controllers");
+const { getCategories, getReviews } = require("./controllers/controllers");
 const { handle500s } = require("./error-handling/erros");
 
-app.use(express.json());
-
 app.get("/api/categories", getCategories);
+app.get("/api/reviews", getReviews)
 app.all("*", handle500s);
 
 module.exports = app;
