758d4b0b05cafa0ac95d8d18e411162299b60035
ahsan-0 <ashanb995@gmail>
2022-12-12 14:43:55 +0000

    dry up code

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,17 +3,18 @@ const app = require("../app");
 const testData = require("../db/data/test-data/index");
 const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed");
+const categories = testData.categoryData;
 beforeEach(() => seed(testData));
 afterAll(() => {
   if (db.end) db.end();
 });
+
 describe("GET /api/categories", () => {
   test("should respond with 200 and get all categories from test data", () => {
     return request(app)
       .get("/api/categories")
       .expect(200)
       .then(({ body }) => {
-        const categories = testData.categoryData;
         expect(body).toEqual({ categories });
       });
   });
