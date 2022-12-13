e7b1e33f63df7242140965d698aa105bbfeb7811
ahsan-0 <ashanb995@gmail>
2022-12-13 15:14:10 +0000

    refactor both GETs tests to use forEach and also use destructuring to get data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,7 +15,22 @@ describe("GET /api/categories", () => {
       .get("/api/categories")
       .expect(200)
       .then(({ body }) => {
-        expect(body).toEqual({ categories });
+        const { categories } = body;
+        categories.forEach((category) => {
+          expect(category).toMatchObject({
+            slug: expect.any(String),
+            description: expect.any(String),
+          });
+        });
+      });
+  });
+  test("categories array should have a length of 4", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        const { categories } = body;
+        expect(categories).toHaveLength(4);
       });
   });
 });
@@ -26,30 +41,29 @@ describe("GET /api/reviews", () => {
       .get("/api/reviews")
       .expect(200)
       .then(({ body }) => {
-        expect(body.reviews[5]).toEqual({
-          review_id: 2,
-          title: "Jenga",
-          category: "dexterity",
-          designer: "Leslie Scott",
-          owner: "philippaclaire9",
-          review_body: "Fiddly fun for all the family",
-          review_img_url: "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
-          created_at: "2021-01-18T10:01:41.251Z",
-          votes: 5,
-          comment_count: "3",
+        const { reviews } = body;
+        reviews.forEach((review) => {
+          expect(review).toMatchObject({
+            review_id: expect.any(Number),
+            title: expect.any(String),
+            owner: expect.any(String),
+            category: expect.any(String),
+            review_img_url: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            designer: expect.any(String),
+            comment_count: expect.any(String),
+          });
         });
-        expect(body.reviews[11]).toEqual({
-          review_id: 3,
-          title: "Ultimate Werewolf",
-          category: "social deduction",
-          designer: "Akihisa Okui",
-          owner: "bainesface",
-          review_body: "We couldn't find the werewolf!",
-          review_img_url: "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
-          created_at: "2021-01-18T10:01:41.251Z",
-          votes: 5,
-          comment_count: "3",
-        });
+      });
+  });
+  test("review array should have a length of 13", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        const { reviews } = body;
+        expect(reviews).toHaveLength(13);
       });
   });
 });
