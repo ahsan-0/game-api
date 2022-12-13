6252adf7262a23abccc996404a1fad3ef764b815
ahsan-0 <ashanb995@gmail>
2022-12-13 13:43:08 +0000

    add selectReviews model and query using JOIN and GROUP BY

--- models/models.js
+++ models/models.js
@@ -4,3 +4,13 @@ exports.selectCategories = () => {
     return rows;
   });
 };
+
+exports.selectReviews = () => {
+  return db
+    .query(
+      `SELECT reviews.*, COUNT(comments.review_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id GROUP BY reviews.review_id;`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
