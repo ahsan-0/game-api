e750c6ad8f79600a6dd44e43e216104ad72ab304
ahsan-0 <ashanb995@gmail>
2022-12-13 16:08:18 +0000

    create /api/reviews/:review_id endpoint

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const { getCategories, getReviews } = require("./controllers/controllers");
+const { getCategories, getReviews, getReviewById } = require("./controllers/controllers");
 const { handle500s } = require("./error-handling/erros");
 
 app.get("/api/categories", getCategories);
-app.get("/api/reviews", getReviews)
-app.get("/api/reviews/:review_id")
+app.get("/api/reviews", getReviews);
+app.get("/api/reviews/:review_id", getReviewById);
 app.all("*", handle500s);
 
 module.exports = app;
