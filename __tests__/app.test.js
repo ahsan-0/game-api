0097d21719d83c093bd1a1d4e54e16fb9b19ec47
ahsan-0 <ashanb995@gmail>
2022-12-13 13:41:42 +0000

    add tests for GET api/reviews

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,3 +19,37 @@ describe("GET /api/categories", () => {
       });
   });
 });
+
+describe("GET /api/reviews", () => {
+  test("should respond with 200 and get all reviews with an added key of comment count", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.reviews[5]).toEqual({
+          review_id: 2,
+          title: "Jenga",
+          category: "dexterity",
+          designer: "Leslie Scott",
+          owner: "philippaclaire9",
+          review_body: "Fiddly fun for all the family",
+          review_img_url: "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+          created_at: "2021-01-18T10:01:41.251Z",
+          votes: 5,
+          comment_count: "3",
+        });
+        expect(body.reviews[11]).toEqual({
+          review_id: 3,
+          title: "Ultimate Werewolf",
+          category: "social deduction",
+          designer: "Akihisa Okui",
+          owner: "bainesface",
+          review_body: "We couldn't find the werewolf!",
+          review_img_url: "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+          created_at: "2021-01-18T10:01:41.251Z",
+          votes: 5,
+          comment_count: "3",
+        });
+      });
+  });
+});
