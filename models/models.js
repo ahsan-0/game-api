cbacee38667dc373bdcb71e19d8700c2ca8ebe1b
ahsan-0 <ashanb995@gmail>
2022-12-13 16:32:44 +0000

    add order by clause to order by date

--- models/models.js
+++ models/models.js
@@ -8,7 +8,7 @@ exports.selectCategories = () => {
 exports.selectReviews = () => {
   return db
     .query(
-      `SELECT reviews.review_id,reviews.title,reviews.owner,reviews.category,reviews.review_img_url,reviews.created_at,reviews.votes,reviews.designer, COUNT(comments.review_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id GROUP BY reviews.review_id;`
+      `SELECT reviews.review_id,reviews.title,reviews.owner,reviews.category,reviews.review_img_url,reviews.created_at,reviews.votes,reviews.designer, COUNT(comments.review_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id GROUP BY reviews.review_id ORDER BY reviews.created_at desc;`
     )
     .then(({ rows }) => {
       return rows;
