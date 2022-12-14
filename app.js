c62678aefb46f67735e1254ad0bf1697c7fab751
ahsan-0 <ashanb995@gmail>
2022-12-14 11:06:07 +0000

    add 2 error handlers & change 500 error handler to a 404

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const { getCategories, getReviews, getReviewById } = require("./controllers/controllers");
-const { handle500s } = require("./error-handling/erros");
+const { handle404s, idDoesNotExistError, invalidIdError } = require("./error-handling/erros");
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id", getReviewById);
-app.all("*", handle500s);
-
+app.all("*", handle404s);
+app.use(idDoesNotExistError);
+app.use(invalidIdError);
 module.exports = app;
