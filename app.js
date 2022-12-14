3a8cfbd35de692aa12cff1a72fe58ac0bd9a15bf
ahsan-0 <ashanb995@gmail>
2022-12-14 11:59:49 +0000

    refactor error names for better readability

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const { getCategories, getReviews, getReviewById } = require("./controllers/controllers");
-const { handle404s, idDoesNotExistError, invalidIdError } = require("./error-handling/erros");
+const { handle404s, handleCustomErros, handleSQLErrors } = require("./error-handling/erros");
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id", getReviewById);
 app.all("*", handle404s);
-app.use(idDoesNotExistError);
-app.use(invalidIdError);
+app.use(handleCustomErros);
+app.use(handleSQLErrors);
 module.exports = app;
