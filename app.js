b886f9d0e0816d5369b90fc35e7c61356233f2f4
ahsan-0 <ashanb995@gmail>
2022-12-15 12:28:19 +0000

    require controller and add endpoint

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const { getCategories, getReviews, getReviewById } = require("./controllers/controllers");
+const { getCategories, getReviews, getReviewById, getCommentsByReviewId } = require("./controllers/controllers");
 const { handle404s, handleCustomErros, handleSQLErrors, handle500s } = require("./error-handling/erros");
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id", getReviewById);
+app.get("/api/reviews/:review_id/comments", getCommentsByReviewId);
 app.all("*", handle404s);
 app.use(handleCustomErros);
 app.use(handleSQLErrors);
