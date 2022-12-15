45783be438dae92d7f11f04fdec3ee937c033d7b
ahsan-0 <ashanb995@gmail>
2022-12-15 10:13:05 +0000

    fix nesting between models

--- models/models.js
+++ models/models.js
@@ -5,14 +5,6 @@ exports.selectCategories = () => {
   });
 };
 
-exports.selectReviewById = (review_id) => {
-  return db.query(`SELECT * FROM reviews WHERE review_id = $1`, [review_id]).then(({ rows }) => {
-    const review = rows[0];
-    if (!review) {
-      return Promise.reject({ status: 404, msg: "Id does not exist" });
-    }
-    return review;
-  });
 exports.selectReviews = () => {
   return db
     .query(
@@ -21,4 +13,14 @@ exports.selectReviews = () => {
     .then(({ rows }) => {
       return rows;
     });
+};
+
+exports.selectReviewById = (review_id) => {
+  return db.query(`SELECT * FROM reviews WHERE review_id = $1`, [review_id]).then(({ rows }) => {
+    const review = rows[0];
+    if (!review) {
+      return Promise.reject({ status: 404, msg: "Id does not exist" });
+    }
+    return review;
+  });
 };
