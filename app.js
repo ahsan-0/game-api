9989a49f100160595800abeb0c0c5f94f8d5165f
ahsan-0 <ashanb995@gmail>
2022-12-15 10:03:22 +0000

    require in and app.use 500 error handler

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const { getCategories, getReviews, getReviewById } = require("./controllers/controllers");
-const { handle404s, handleCustomErros, handleSQLErrors } = require("./error-handling/erros");
+const { handle404s, handleCustomErros, handleSQLErrors, handle500s } = require("./error-handling/erros");
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
@@ -9,4 +9,5 @@ app.get("/api/reviews/:review_id", getReviewById);
 app.all("*", handle404s);
 app.use(handleCustomErros);
 app.use(handleSQLErrors);
+app.use(handle500s);
 module.exports = app;
