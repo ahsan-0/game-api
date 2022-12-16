5a813bebc3602a9e4c9fb132f0415c88e3093223
ahsan-0 <ashanb995@gmail>
2022-12-16 14:06:09 +0000

    require controller and add GET /api/users endpoint

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const { getCategories, getReviews, getReviewById, getCommentsByReviewId } = require("./controllers/controllers");
+const { getCategories, getReviews, getReviewById, getCommentsByReviewId, getUsers } = require("./controllers/controllers");
 const { handle404s, handleCustomErros, handleSQLErrors, handle500s } = require("./error-handling/erros");
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id", getReviewById);
 app.get("/api/reviews/:review_id/comments", getCommentsByReviewId);
+app.get("/api/users", getUsers);
 app.all("*", handle404s);
 app.use(handleCustomErros);
 app.use(handleSQLErrors);
