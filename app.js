644ed4311bce18ad7750d154fd6425a88cc15697
ahsan-0 <ashanb995@gmail>
2022-12-16 11:10:32 +0000

    fix typo for handleCustomErrors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const { getCategories, getReviews, getReviewById, getCommentsByReviewId, postComment } = require("./controllers/controllers");
-const { handle404s, handleCustomErros, handleSQLErrors, handle500s } = require("./error-handling/erros");
+const { handle404s, handleCustomErrors, handleSQLErrors, handle500s } = require("./error-handling/erros");
 
 app.use(express.json());
 
@@ -11,7 +11,7 @@ app.get("/api/reviews/:review_id", getReviewById);
 app.get("/api/reviews/:review_id/comments", getCommentsByReviewId);
 app.post("/api/reviews/:review_id/comments", postComment);
 app.all("*", handle404s);
-app.use(handleCustomErros);
+app.use(handleCustomErrors);
 app.use(handleSQLErrors);
 app.use(handle500s);
 module.exports = app;
