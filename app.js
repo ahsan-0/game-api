757a5ce6d51c483a0a3fa74a7c103a8f3ab0a72e
ahsan-0 <ashanb995@gmail>
2023-01-09 15:09:13 +0000

    move app.use to correctly work and initialise

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-app.use(cors());
 const app = express();
 
 const { getCategories, getReviews, getReviewById, getCommentsByReviewId, postComment, patchReview, getUsers } = require("./controllers/controllers");
 const { handle404s, handleCustomErrors, handleSQLErrors, handle500s } = require("./error-handling/erros");
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/categories", getCategories);
